22b11c45746161fdd823d37eef0b65e07bb289a1
Norix10 <norix.osv@gmail.com>
2026-04-01 12:57:18 +0300

    updated router dependencies

--- app/routers/api.py
+++ app/routers/api.py
@@ -1,11 +1,13 @@
-from app.routers.v1 import health, user
+from app.routers.v1 import admin, auth, filter, health, nofication, user
 
 from fastapi import APIRouter
 
 api_router = APIRouter(prefix="/api/v1")
 
-# api_router.include_router(booking.router)
+api_router.include_router(admin.router)
+api_router.include_router(auth.router)
+api_router.include_router(filter.router)
 api_router.include_router(health.router)
-
+api_router.include_router(nofication.router)
 api_router.include_router(user.router)
-# api_router.include_router()
+
